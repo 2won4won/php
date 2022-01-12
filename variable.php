59efa6567c5814f6bc1cbb7ce6535c222a2ad7e2
won <2won4won@gmail.com>
2022-01-12 18:17:06 +0900

    add variable

---
+++ variable.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1> variable </h1>
+  <!-- <?php 
+    $a = 1;
+    echo $a + 3;
+  ?> -->
+  <?php
+    $name="123";
+      echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Odit quidem" .$name." id suscipit illo eos vel deserunt veniam? 
+      Quasi praesentium quiorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Odit quidem id suscipit illo eos vel deserunt veniam? 
+      Quasi praese" .$name." ntium quiorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Odit quidem id suscipit illo eos " .$name." vel deserunt veniam? 
+      Quasi praesentium qui adipisci explicabo, cupiditate ipsum, 
+      nobis saepe omnis maiores hic quas." 
+  ?>
+</body>
+</html>
+<style>
+  
+</style>
