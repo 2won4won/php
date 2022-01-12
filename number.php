5410ed8cfd1fad5beab978abb7893932bc38bc35
won <2won4won@gmail.com>
2022-01-12 18:03:43 +0900

    first commit

---
+++ number.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <?php
+      // echo 6;
+      // print(2);
+      echo 1+7;
+     ?>
+  </body>
+</html>
---
+++ string.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>
+      <?php
+        echo "Hello \"PH\"P";
+       ?>
+   </h1>
+   <h1>
+    <?php
+      echo "hello "."world";
+     ?>
+   </h1>
+   <h1>
+    <?php
+      echo strlen("unopinionated");
+     ?>
+   </h1>
+  </body>
+</html>
